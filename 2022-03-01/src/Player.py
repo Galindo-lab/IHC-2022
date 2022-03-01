ffb21752c8a1d6372b41e67d50b8b87161a16570
Luis Galindo <egalindo54@uabc.edu.mx>
2022-03-01 12:18:28 -0800

    controles de movimiento agregados

--- 2022-03-01/src/Player.py
+++ 2022-03-01/src/Player.py
@@ -6,7 +6,8 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self,x,y):
         self.position = pygame.math.Vector2(x,y)
-        self.rect = pygame.Rect( (x,y), (20,20) )
+        self.change = pygame.math.Vector2(0,0)
+        self.rect = pygame.Rect( (x,y), (40,40) )
         self.max_speed = 5
         self.color = (255,0,0)
 
@@ -15,4 +16,19 @@ class Player(pygame.sprite.Sprite):
         
 
     def update(self, dt):
-        
+        self.position += self.change * dt
+        self.change.x = 0
+        self.change.y = 0
+        pass
+
+    def move_left(self):
+        self.change.x = -self.max_speed
+
+    def move_right(self):
+        self.change.x = self.max_speed
+
+    def move_up(self):
+        self.change.y = -self.max_speed
+
+    def move_down(self):
+        self.change.y = -self.max_speed
